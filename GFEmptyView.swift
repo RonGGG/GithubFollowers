115dd32f27d041ae96c26520b4dfd5cba6e21229
Ron <ronguozirong@icloud.com>
2024-01-23 00:23:51 +1100

    Something wrong with the cell display Create GFEmptyView Create loadingView & add loading function for FollowerListVC Create EmptyStateView

---
+++ GFEmptyView.swift
@@ -0,0 +1,20 @@
+//
+//  GFEmptyView.swift
+//  GHFFollowers
+//
+//  Created by 郭梓榕 on 22/1/2024.
+//
+
+import UIKit
+
+class GFEmptyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
